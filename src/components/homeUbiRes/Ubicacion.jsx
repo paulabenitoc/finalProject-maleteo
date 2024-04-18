2ad913273b90cbddfc70ec0e8be50bc44d1ae931
FlorencioTom <florenciotom@hotmail.com>
2024-04-18 18:25:26 +0200

    he añadido cosas a mi componente homeUbiRes

--- src/components/homeUbiRes/Ubicacion.jsx
+++ src/components/homeUbiRes/Ubicacion.jsx
@@ -1,29 +1,35 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, createContext, useContext, useEffect } from 'react';
 import { useSpring, animated, config } from 'react-spring';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import {NavLink} from 'react-router-dom';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { HomeUbiResContext } from './HomeUbiResContext';
+//import useMiContexto  from './HomeUbiResContext';
 import mapboxgl from 'mapbox-gl';
-import ScrollUpButton from "react-scroll-up";
-import lupa from '../../public/images/lupa.png';
-import atras from '../../public/images/atras.png';
-import aspa from '../../public/images/aspa.png';
-import brujula from '../../public/images/brujula.png';
-import reloj from '../../public/images/reloj.png';
-import equis from '../../public/images/equis.png';
-import imgBtn from '../../public/images/img-btn.png';
-import calendario from '../../public/images/calendario.png';
-import maleta from '../../public/images/maleta.png';
+import axios from 'axios';
+import aspa from '../../../public/images/aspa.png';
+import brujula from '../../../public/images/brujula.png';
+import reloj from '../../../public/images/reloj.png';
+import equis from '../../../public/images/equis.png';
+import imgBtn from '../../../public/images/img-btn.png';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './HomeUbiRes.css';
 
 const Ubicacion = () => {
-  const [ciudad, setCiudad] = useState('');
+  //const [ciudad, setCiudad] = useState('');
+  const {ciudad} = useContext(HomeUbiResContext);
+  const {setCiudad} = useContext(HomeUbiResContext);
   const [mapa, setMapa] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
-  const mapContainer = useRef(null);
+  const [error, setError] = useState(null);
+  const inputCiudad = useRef('');
+  //const {ciudad} = useMiContexto();
+  //const {setCiudad } = useMiContexto();
 
   const initialGradient = 'linear-gradient(to right, #0000, #0000)';  
   const finalGradient = 'linear-gradient(to right,#feb030,#ffce32)';  
+
+  const ubicacionContexto = createContext();
 
   // Usa useSpring para animar el gradiente
   const { background } = useSpring({
@@ -33,93 +39,97 @@ const Ubicacion = () => {
   });
   //const [amarillo, setAmarillo] = useState('container');
 
-  const cambiaColor = () => {
-    setMapa(!mapa);
-  }
-  useEffect(() => {
+  const miUbicacion = () => {
     mapboxgl.accessToken = 'pk.eyJ1IjoiZmxvb3dlZW4iLCJhIjoiY2x2NGF3OXY0MDY3dzJxbW5tNG1jeDVwZiJ9.NDhhbLKm9mhhzRbaFVdSDA';
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v12',
-      center: [-3.7038, 40.4168],
-      zoom: 15,
-    }).on('load', () => {
-      map.resize();
-      console.log(map);
-    });
+    const obtenerCiudadDesdeCoordenadas = async (lng, lat) => {
+      try {
+        const response = await axios.get(
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?types=place&access_token=${mapboxgl.accessToken}`
+        );
 
-    const marker = new mapboxgl.Marker({
-      color: "#F68F24"
-    })
-    .setLngLat([-3.7038, 40.4168])
-    .addTo(map);
+        // Extrae la ciudad de la respuesta
+        const features = response.data.features;
+        if (features.length > 0) {
+          const ciudadd = features[0].text;
+          console.log(ciudadd, features);
+          inputCiudad.current.value = ciudadd;
+          
+        } else {
+          setError('No se encontró la ciudad.');
+        }
+      } catch (error) {
+        setError('No se pudo obtener la información de ubicación.');
+        console.error(error);
+      }
+    };
 
-    if (mapa) {
-      document.documentElement.classList.add('hide-scrollbar');
-    } else {
-      document.documentElement.classList.remove('hide-scrollbar');
-    }
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { longitude, latitude } = position.coords;
+        obtenerCiudadDesdeCoordenadas(longitude, latitude);
+      },
+      (error) => {
+        setError('No se pudo obtener la ubicación del usuario.');
+        console.error(error);
+      }
+    );
+  }
 
-    return () => map.remove();
-  }, [mapa]);
-
+  const borrar = () => {
+    inputCiudad.current.value = '';
+  }
 
   return (
     <>    
     <div className='ciudad'>
       <animated.div className='container' style={{ background }}>
         <div className='top'>
-          <FontAwesomeIcon className={`fa-flecha ${mapa?'blanca':''}`} icon={faChevronLeft} />
+          <NavLink to="/reserva">
+            <FontAwesomeIcon className={`fa-flecha ${mapa?'blanca':''}`} icon={faChevronLeft} />
+          </NavLink>
           <h1 className='center-text-white'>¿En que zona?</h1>
         </div>
         <div className='buscadores mt-2 mb-2'>
           <div className='buscador sombra'>
             <img className='icono lupa' src={aspa} alt="lupa" />
-            <input className='input-buscador' type="text" placeholder='Donde te encuentras? Madrid, Barcelona...'/>
-            <img className='icono equis' src={equis} alt="equis para borrar texto" />
+            <input className='input-buscador' ref={inputCiudad} type="text" placeholder='Donde te encuentras? Madrid, Barcelona...'/>
+            <img className='icono equis' src={equis} alt="equis para borrar texto" onClick={() => borrar()}/>
           </div>
         </div>
       </animated.div>
-      {!mapa &&
       <div className='respuestas'>
-      <div className='buscame'>
-          <img className='icono brujula' src={brujula} alt="lupa" />
+      <div className='buscame miUbi' onClick={() => miUbicacion()}>
+          <img className='icono brujula' src={brujula} alt="brujula" />
           <p className='text'>Utilizar mi ubicacion actual</p>
       </div>     
       
-      <div className='buscame'>
-          <img className='icono reloj' src={reloj} alt="lupa" />
+      <div className='buscame miUbi'>
+          <img className='icono reloj' src={reloj} alt="reloj" />
           <p className='text'>Pacifico, Madrid</p>
       </div> 
       <hr className='divisor' />
-      <div className='buscame'>
-          <img className='icono reloj' src={reloj} alt="lupa" />
+      <div className='buscame miUbi'>
+          <img className='icono reloj' src={reloj} alt="reloj" />
           <p className='text'>Alicante, España</p>
       </div> 
       <hr className='divisor' />
-      <div className='sugerencias'>
+      <div className='sugerencias miUbi'>
           <p className='text'>Madrid</p>
           <p className='text light'>España</p>
       </div> 
       <hr className='divisor' />
-      <div className='sugerencias'>
+      <div className='sugerencias miUbi'>
           <p className='text'>Madrid -Atocha cercanías</p>
           <p className='text light'>Madrid, España</p>
       </div> 
       <hr className='divisor' />
     </div>      
-      }
     
-    <div className='next' onClick={() => {cambiaColor()}}>
+    <NavLink to="/reserva/ubicacion/ubicaciones" onClick={() => {setCiudad(inputCiudad.current.value)}} className='next'>
       <img className='btn-next' src={imgBtn} alt="" />
-    </div>
+    </NavLink>
     
     </div>
-    <div>
-      
-    </div>
-    <div id='map' className={`${mapa?'d-block':'d-none'}`}></div>    
-    
     </>
   )
 }
