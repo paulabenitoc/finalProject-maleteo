6d6685c7a26f086d2f43f6f353336de577a4670f
stUpGrade <160643466+stUpGrade@users.noreply.github.com>
2024-04-19 17:18:34 +0200

    avance en anuncios

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
 import './App.css'
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+<<<<<<< Updated upstream
+=======
+import Home from './pages/home/home.jsx';
+import Confirmacion from './pages/Confirmacion/Confirmacion';
+import Usuario from './pages/Usuario/Usuario.jsx';
+import HomeUbiRes from './components/homeUbiRes/HomeUbiRes';
+import Ubicacion from './components/homeUbiRes/Ubicacion';
+import Ubicaciones from './components/homeUbiRes/Ubicaciones';
+import {HomeUbiResContext} from './components/homeUbiRes/HomeUbiResContext';
+import Espacio from './pages/servicios/Espacio.jsx';
+import Servicios from './pages/servicios/Servicios.jsx';
+
+>>>>>>> Stashed changes
 
 
 function App() {
@@ -10,11 +23,22 @@ function App() {
         
 
         <Routes>
+<<<<<<< Updated upstream
           
           <Route>
 
           </Route>
 
+=======
+          <Route path='/home' element={<Home />} />
+          <Route path='/confirmacion' element={<Confirmacion />} />
+          <Route path='/usuario' element={<Usuario/>} />
+          <Route path='/reserva' element={<HomeUbiRes/>} />
+          <Route path='/reserva/ubicacion' element={<Ubicacion/>} />
+          <Route path='/reserva/ubicacion/ubicaciones' element={<Ubicaciones />} />
+          <Route path='/espacio' element={<Espacio />} /> 
+          <Route path='/servicios' element={<Servicios />} />  
+>>>>>>> Stashed changes
         </Routes>
 
 
---
+++ src/pages/servicios/Espacio.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import Volver from '../../components/Volver/Volver';
+import './Espacio.css';
+
+function Espacio() {
+  const [mostrarOpciones1, setMostrarOpciones1] = useState(false);
+  const [opcionSeleccionada1, setOpcionSeleccionada1] = useState(null);
+  const [mostrarOpciones2, setMostrarOpciones2] = useState(false);
+  const [opcionSeleccionada2, setOpcionSeleccionada2] = useState(null);
+
+  const toggleOpciones1 = () => {
+    setMostrarOpciones1(!mostrarOpciones1);
+  };
+
+  const toggleOpciones2 = () => {
+    setMostrarOpciones2(!mostrarOpciones2);
+  };
+
+  const seleccionarOpcion1 = (opcion) => {
+    setOpcionSeleccionada1(opcion);
+    setMostrarOpciones1(false); // Ocultar las opciones después de seleccionar
+  };
+
+  const seleccionarOpcion2 = (opcion) => {
+    setOpcionSeleccionada2(opcion);
+    setMostrarOpciones2(false); // Ocultar las opciones después de seleccionar
+  };
+
+  return (
+    <div>
+      <Volver />
+      <div className='title_box'>
+        <h1 className='title_text'>Describenos tu espacio</h1>
+      </div>
+      <div>
+        <button className='button' onClick={toggleOpciones1}>
+          Especifica tu propiedad
+        </button>
+        {mostrarOpciones1 && (
+          <div className='opciones'>
+            <p onClick={() => seleccionarOpcion1('Casa')}>Casa</p>
+            <p onClick={() => seleccionarOpcion1('Hotel')}>Hotel</p>
+            <p onClick={() => seleccionarOpcion1('Establecimiento')}>
+              Establecimiento
+            </p>
+          </div>
+        )}
+        <p>{!opcionSeleccionada1 ? 'Selecciona una opción' : opcionSeleccionada1}</p>
+          </div>
+          <hr />
+        <div>
+        <button className='button' onClick={toggleOpciones2}>
+          ¿Qué tipo de espacio?
+        </button>
+        {mostrarOpciones2 && (
+          <div className='opciones'>
+            <p onClick={() => seleccionarOpcion2('Habitacion')}>Habitacion</p>
+            <p onClick={() => seleccionarOpcion2('Hall')}>Hall</p>
+            <p onClick={() => seleccionarOpcion2('Trastero')}>Trastero</p>
+            <p onClick={() => seleccionarOpcion2('Buhardilla')}>Buhardilla</p>
+            <p onClick={() => seleccionarOpcion2('Garaje')}>Garaje</p>
+          </div>
+        )}
+        <p>{!opcionSeleccionada2 ? 'Selecciona una opción' : opcionSeleccionada2}</p>
+          </div>
+          <hr />
+    </div>
+  );
+}
+
+export default Espacio;
