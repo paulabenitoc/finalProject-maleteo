49d4f93cc06957f8717838aa5e088a604a1f74cd
paul <paulboar.a@gmail.com>
2024-04-19 19:38:17 +0200

    reserva

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,10 @@ import Usuario from './pages/Usuario/Usuario.jsx';
 import HomeUbiRes from './components/homeUbiRes/HomeUbiRes';
 import Ubicacion from './components/homeUbiRes/Ubicacion';
 import Ubicaciones from './components/homeUbiRes/Ubicaciones';
-import {HomeUbiResContext} from './components/homeUbiRes/HomeUbiResContext';
+import { HomeUbiResContext } from './components/homeUbiRes/HomeUbiResContext';
+import { PrimeReactProvider } from 'primereact/api';
+import Calendario from './pages/Reserva/Calendario.jsx';
+import MapaUbicacion from './pages/Reserva/Mapaubicacion.jsx';
 
 
 
@@ -15,18 +18,24 @@ function App() {
   const [ciudad, setCiudad] = useState('');
   return (
     <>
-    <HomeUbiResContext.Provider value={{ ciudad, setCiudad }}>
-      <Router>
-        <Routes>
-          <Route path='/home' element={<Home />} />
-          <Route path='/confirmacion' element={<Confirmacion />} />
-          <Route path='/usuario' element={<Usuario/>} />
-          <Route path='/reserva' element={<HomeUbiRes/>} />
-          <Route path='/reserva/ubicacion' element={<Ubicacion/>} />
-          <Route path='/reserva/ubicaciones' element={<Ubicaciones/>} />
-        </Routes>
-      </Router>
-    </HomeUbiResContext.Provider>
+      <PrimeReactProvider>
+        <HomeUbiResContext.Provider value={{ ciudad, setCiudad }}>
+
+          <Router>
+            <Routes>
+              <Route path='/home' element={<Home />} />
+              <Route path='/confirmacion' element={<Confirmacion />} />
+              <Route path='/usuario' element={<Usuario />} />
+              <Route path='/reserva' element={<HomeUbiRes />} />
+              <Route path='/reserva/ubicacion' element={<Ubicacion />} />
+              <Route path='/reserva/ubicaciones' element={<Ubicaciones />} />
+              <Route path='/calendario' element={<Calendario />} />
+              <Route path='/mapaubicacion' element={<MapaUbicacion />} />
+            </Routes>
+          </Router>
+        </HomeUbiResContext.Provider>
+      </PrimeReactProvider>
+
     </>
   )
 }
---
+++ src/pages/Reserva/thanks.jsx
@@ -0,0 +1,16 @@
+
+export default function Thanks() {
+    return (
+        <>
+            <div>
+                <h1>Reserva Completada</h1>
+                <img src='public/image/fenix@3x.png'></img>
+            </div>
+
+            <div>
+                <h2>BE FREE!</h2>
+                <p>Contacta ya con tu guardían y espera a que acepte tu reserva.</p>
+            </div>
+        </>
+    )
+}
