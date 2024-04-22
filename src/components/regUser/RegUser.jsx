7f2b9efdde7be11171808d4d5ad53af3bdcffa6d
Gokenve <gokenve@gmail.com>
2024-04-22 11:40:50 +0200

    feat_almost_final2_upgrade

--- src/components/regUser/RegUser.jsx
+++ src/components/regUser/RegUser.jsx
@@ -2,51 +2,80 @@
 import "./regUser.css";
 
 // eslint-disable-next-line react/prop-types
-const LoginUser = ({ handleInputs, usuario, errorTypes }) => {
+const RegUser = ({ handleInputs, usuario }) => {
   return (
-    <div className="login-inputs">
-      <form>
-        <label className="login_txt1" htmlFor="email">
-          Dirección de correo electrónico
-        </label>
+    <div className="register-inputs">
+      <label className="reg_txt1" htmlFor="email">
+        Dirección de correo electrónico
+      </label>
+      <input
+        className="reg_inputs"
+        type="email"
+        value={usuario.email}
+        id="email"
+        onChange={handleInputs}
+        pattern="/^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/"
+      ></input>
 
-        <input
-          className="log_inputs"
-          type="email"
-          value={usuario.email}
-          id="email"
-          onChange={handleInputs}
-          required
-          pattern="/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;"
-        ></input>
-        {errorTypes.errorLogin ? (
-          <p style={{ color: "red", fontSize: "12px" }}>Usuario incorrecto</p>
-        ) : (
-          <p></p>
-        )}
+      <label className="reg_txt" htmlFor="nombre">
+        Nombre
+      </label>
+      <input
+        className="reg_inputs"
+        type="text"
+        value={usuario.nombre}
+        id="nombre"
+        onChange={handleInputs}
+      ></input>
 
-        <label className="login_txt" htmlFor="contraseña">
-          Contraseña
-        </label>
-        <p></p>
+      <label className="reg_txt" htmlFor="apellidos">
+        Apellido
+      </label>
+      <input
+        className="reg_inputs"
+        type="text"
+        value={usuario.apellidos}
+        id="apellidos"
+        onChange={handleInputs}
+      ></input>
+
+      <label className="reg_txt" htmlFor="contraseña">
+        Contraseña
+      </label>
+      <input
+        className="reg_inputs"
+        type="password"
+        value={usuario.contraseña}
+        id="contraseña"
+        onChange={handleInputs}
+        pattern="/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$/"
+      ></input>
+
+      <label className="reg_txt" htmlFor="fecha_nacimiento">
+        Fecha de nacimiento
+      </label>
+      <input
+        className="reg_inputs"
+        type="date"
+        value={usuario.fecha_nacimiento}
+        id="fecha_nacimiento"
+        onChange={handleInputs}
+        placeholder="Para registrarte tendrás que ser mayor de edad. Los usuarios no veran tu fecha de cumpleaños"
+      ></input>
+      <div className="check_area">
         <input
-          className="log_inputs"
-          type="password"
-          value={usuario.contraseña}
-          id="contraseña"
+          className="check"
+          type="checkbox"
+          id="notificacion"
           onChange={handleInputs}
-          required
         ></input>
-        {errorTypes.errorMessage ? (
-          <p style={{ color: "red", fontSize: "12px" }}>
-            {errorTypes.errorMessage}
-          </p>
-        ) : (
-          <p></p>
-        )}
-      </form>
+        <label className="check_text" htmlFor="notificacion">
+          Quiero recibir consejos sobre como gestionar mi equipaje, ofertas,
+          novedades y totros correos electrónicos de Maleteo
+        </label>
+      </div>
     </div>
   );
 };
 
-export default LoginUser;
+export default RegUser;
