8bcda370b7d4f2ffcca873f63ca9a2efab9d42c2
paulabenitoc <paulabenitocapellan@gmail.com>
2024-04-22 10:53:07 +0200

    rutas nav

--- src/components/GuardianNav/GuardianNav.jsx
+++ src/components/GuardianNav/GuardianNav.jsx
@@ -8,22 +8,22 @@ const GuardianNav = () => {
     <nav className='guardian-menu'>
       <ul className='guardian-bar'>
         <li className='guardian-item'>
-          <Link to="/chat">
+          <Link to="/confirmacion">
             <img src="public/img/mensaje@2x.png" alt="chat icon" className='guardian-img' />
           </Link>
         </li>
         <li className='guardian-item'>
-          <Link to="/alert">
+          <Link to="/ficha">
             <img src="public/img/altavoz.png" alt="alert icon" className='guardian-img'/>
           </Link>
         </li>
         <li className='guardian-item'>
-          <Link to="/calendar">
+          <Link to="/confirmacion">
             <img src="public/img/calendario.png" alt="calendar icon" className='guardian-img'/>
           </Link>
         </li>
         <li className='guardian-item'>
-          <Link to="/user">
+          <Link to="/usuario">
             <img src="public/img/usuario@2x.png" alt="user icon" className='guardian-img'/>
           </Link>
         </li>
